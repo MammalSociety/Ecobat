5b69308a804c602a77ab60d1761f1d276446a600
Bethany Smith B <brs28@sussex.ac.uk>
2019-02-19 18:06:04 +0000

    small changes

--- Nightly/App-1/server.R
+++ Nightly/App-1/server.R
@@ -21,33 +21,33 @@ shinyServer(function(input, output) {
     if (is.null(data1)) #if no data has been uploaded, main panel is empty
       return(NULL)
     
-    data2 < -read.csv(data1$datapath, header=TRUE)
+    data2 <- read.csv(data1$datapath, header=TRUE)
     data2 #if data has been uploaded, it reads it and then displays in the main panel
   })
   
-  # this reactive output tells Shiny that once data has been uploaded it should display a download button for the
+  # this reactive output tells Shiny that once data has been uploaded it should display a   download button for the
   # Rmd report. If no data has been uploaded then the button will not appear.
   output$ui.download <- renderUI({
     if (is.null(input$file)) return() #if no data uploaded button is not present
     downloadButton("report", "Generate report")}) #if data uploaded button appears
   
   output$ui.download.helper <- renderUI({
-    if (is.null(input$file)) return() #if no data uploaded, helper text associated w/ download button not present
+    if (is.null(input$file)) return() #if no data uploaded, helper text associated with      the download button not present
     h5(helpText("Report may take a few minutes to generate; please be patient."))})
   #if data uploaded, helper text associated with the download button appears
   
   output$report <- downloadHandler(
-    # For PDF output, change this to "report.pdf" #pdf output requires LaTex
-    filename = "Ecobat Report.doc", #we want word document output
+    filename = "Ecobat Nightly Report.doc", #we want word document output
+    
     content = function(file) {
       # Copy the report file to a temporary directory before processing it, in
       # case we don't have write permissions to the current working dir (which
       # can happen when deployed).
-      tempReport <- file.path(tempdir(), "Nightly.Rmd") #change this to EcobatScript.Rmd when app
-      #is deployed
-      file.copy("Nightly.Rmd", tempReport, overwrite = TRUE) #also change this
+      tempReport <- file.path(tempdir(), "Nightly.Rmd") 
+      file.copy("Nightly.Rmd", tempReport, overwrite = TRUE)
       
-      #**CRUCIAL CODE this code is responsible for passing the data to Rmd.
+      #**CRUCIAL CODE
+      # this code is responsible for passing the data to Rmd.
       dataa<-input$file #tells R Markdown where it can find data
       datab<-read.csv(dataa$datapath, header=TRUE) #tells Rmd where to read the data from
       author<-input$Author #tells Rmd what to use as Author
@@ -56,13 +56,67 @@ shinyServer(function(input, output) {
       # Set up parameters to pass to Rmd document
       params <- list(n = datab, Author=author)
       
+      word_document <- function(toc = FALSE,
+                                toc_depth = 3,
+                                fig_width = 5,
+                                fig_height = 4,
+                                fig_caption = TRUE,
+                                df_print = "default",
+                                smart = TRUE,
+                                highlight = "default",
+                                reference_docx = "stylesdoc.docx",
+                                keep_md = FALSE,
+                                md_extensions = NULL,
+                                pandoc_args = NULL) {
+        
+        
+        
+        # reference docx
+        args <- c(args, reference_doc_args("docx", reference_docx))
+        
+        
+        
+        saved_files_dir <- NULL
+        pre_processor <- function(metadata, input_file, runtime, knit_meta, files_dir, output_dir) {
+          saved_files_dir <<- files_dir
+          NULL
+        }
+        
+        intermediates_generator <- function(...) {
+          reference_intermediates_generator(saved_files_dir, ..., reference_docx)
+        }
+        
+        # return output format
+        output_format(
+          knitr = knitr,
+          pandoc = pandoc_options(to = "docx",
+                                  from = from_rmarkdown(fig_caption, md_extensions),
+                                  args = args),
+          keep_md = keep_md,
+          df_print = df_print,
+          pre_processor = pre_processor,
+          intermediates_generator = intermediates_generator
+        )
+      }
+      
+      reference_doc_args <- function(type, doc) {
+        if (is.null(doc) || identical(doc, "default")) return()
+        c(paste0("--reference-", if (pandoc2.0()) "doc" else {
+          match.arg(type, c("docx", "odt", "doc"))
+        }), pandoc_path_arg(doc))
+      }
+      
+      
       # Knit the document, passing in the `params` list, and eval it in a
       # child of the global environment (this isolates the code in the document
       # from the code in this app).
-      rmarkdown::render(tempReport, output_file = file,
+      rmarkdown::render(tempReport, word_document(reference_docx = "stylesdoc.docx"),
+                        output_file = file,
                         params = params,
                         envir = new.env(parent = globalenv()))
+                        
       
     })  
   
 })
+
--- Nightly/App-1/ui.R
+++ Nightly/App-1/ui.R
@@ -1,6 +1,6 @@
-#load packages - included all the ones required for the Rmd script to run too - not sure if necessary
-library(shiny) # tell R which packages it's going to need to use
-library(rmarkdown) # ggplot2, knitr, pander and plyr are called in the rmd code
+#load packages - not sure if necessary
+library(shiny) # 
+library(rmarkdown) 
 library(ggplot2)
 library(knitr)
 library(pander)
@@ -9,12 +9,14 @@ library(tidyr)
 library(rcompanion)
 
 ui<-(fluidPage( #fluidpage means page width determined by window size
-  titlePanel("Ecobat Data Analysis"), #title of Shiny app
+  titlePanel("Nightly Bat Activity Analysis"), #title of Shiny app
   sidebarLayout( #we want the layout with a sidebar and a main panel
     sidebarPanel( #options for sidepanel
       
       #*Input() functions,
       #*Output() functions
+      #add Ecobat logo at the top centre
+      h5(img(src= "ecobat-logo.png", heigth=100, width=100, style="display: block; margin-left: auto; margin-right: auto;")),
       
       fileInput(inputId = "file", label = "Upload CSV file", #upload field
                 multiple = FALSE, #can only upload one file at a time
@@ -31,19 +33,12 @@ ui<-(fluidPage( #fluidpage means page width determined by window size
       
       textInput(inputId = "Author", label = "Insert Author Name"), #input field for author name
       
-      #scrapping these checkboxes for now but potential for checkboxes in future other shiny app
-      #so leaving this code here so I can see how to code checkboxes
-      #h5(helpText("Select the parameters below.")),
-      #h5(helpText("If you uploaded weather data to Ecobat please check the 'Weather' box. If you uploaded temperature data to Ecobat please check the 'Temperature' box.")),
-      #checkboxInput(inputId= "Weather", label = "Weather", value = FALSE),
-      #checkboxInput(inputId= "Temperature", label = "Temperature", value = FALSE),
-      
       tags$hr(), #adds a horizontal line
       uiOutput("ui.download"), # instead of conditionalPanel
       uiOutput("ui.download.helper"), #these two lines of code enable conditional appearance of download button
       #and helper text conditional on data having been uploaded
       tags$hr(), #adds a horizontal line
-      h5("Developed by ", img(src= "ecobat-logo.png", heigth=100, width=100)) #gets it to display Ecobat logo
+      h5(img(src= "TMS_logo.png", heigth=100, width=100, style="display: block; margin-left: auto; margin-right: auto;")) #gets it to display Ecobat logo
     ),
     mainPanel(
       uiOutput('contents') #display in main panel dependent on 'contents' in server.R
